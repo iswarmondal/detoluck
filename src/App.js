62f0634abcb6dedd162b16580cde63bda0caa0f7
Iswar <rick731202@gmail.com>
2022-05-27 08:19:03 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import './App.css';
-import { 
-  BrowserRouter,
-  Routes,
-  Route,
- } from 'react-router-dom';
-import Welcome from './pages/Welcome';
-import ImageUpload from './pages/ImageUpload';
-// import Landing from './pages/Landing';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Welcome />}>
-        <Route index element={<Welcome />} />
-      </Route>
-      <Route path="/image-upload" element={<ImageUpload />} />
-    </Routes>
-  </BrowserRouter>
+    <></>
   );
 }
 
